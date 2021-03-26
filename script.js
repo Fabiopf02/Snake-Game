b619b3258c0f8ef77882fb1b2936bcbd138b4674
Fabiopf02 <fabiofranca593@gmail.com>
2021-03-26 16:12:31 -0300

    set snake movements

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
 const box = 32;
-const count = 16;
 const snake = [];
 snake[0] = {
   x: 8 * box,
   y: 8 * box,
 };
+let direction = "right";
 
 function createBackground() {
   context.fillStyle = '#000000';
-  context.fillRect(0, 0, box * count, box * count);
+  context.fillRect(0, 0, box * 16, box * 16);
 }
 
 function createSnake() {
@@ -21,5 +21,28 @@ function createSnake() {
   }
 }
 
-createBackground();
-createSnake();
+function startGame() {
+  createBackground();
+  createSnake();
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  if(direction === "right") snakeX += box;
+  if(direction === "left") snakeX -= box;
+  if(direction === "up") snakeY -= box;
+  if(direction === "down") snakeY += box;
+
+  snake.pop();
+
+  const newHead = {
+    x: snakeX,
+    y: snakeY,
+  };
+
+  snake.unshift(newHead);
+
+}
+
+const game = setInterval(startGame, 100);
+
