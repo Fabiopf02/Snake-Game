54bb157345bdc910ce971958469d8fefb5566c14
Fabiopf02 <fabiofranca593@gmail.com>
2021-03-26 16:52:08 -0300

    Set randomness of food and snake size

--- script.js
+++ script.js
@@ -70,7 +70,14 @@ function startGame() {
   if(direction === "up") snakeY -= box;
   if(direction === "down") snakeY += box;
 
-  snake.pop();
+  if (snakeX !== food.x || snakeY !== food.y) {
+    snake.pop();
+  }
+  else {
+    food.x = Math.floor(Math.random() * 15 + 1) * box;
+    food.y = Math.floor(Math.random() * 15 + 1) * box;
+  }
+
 
   const newHead = {
     x: snakeX,
